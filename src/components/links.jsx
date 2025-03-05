595c0409baa79dbb0400976c050fc254bf3a512c
Johndillerman <U2486892@rts-365.dk>
2025-03-05 12:47:30 +0100

    shop links

--- src/components/links.jsx
+++ src/components/links.jsx
@@ -2,32 +2,112 @@
 export default function Productlinks() {
     return (
         <>
-        <section className="shop">
-         <div className="shop__wrapper">
-        <div className="shop__grid">
-        {[
-          "Shop by Brand",
-          "Amplifiers",
-          "Speakers",
-          "Turntables",
-          "CD Players",
-          "Streamers",
-          "Cables",
-          "Furniture",
-          "Headphones",
-          "Home Cinema",
-          "Outlet - Save up to 50% Off",
-          "Ebay Shop",
-          "Ceiling Speakers",
-          "TVs",
-          "Vinyl LPs"
-        ].map((item, index) => (
-          <div key={index} className="shop__grid-item">
-            <span className="shop__grid-text">{item}</span>
-          </div>
-        ))}
-      </div>
-      </div>
+       <section class="grid-container" aria-label="Audio Shop Categories">
+        <div class="grid-item">
+          <a href="/Shop-category.html">
+            <img src="Assets/img/Shop images/eBay Shop.jpg" alt="eBay Shop" />
+            <figcaption>shop by brand</figcaption>
+          </a>
+        </div>
+        <div class="grid-item">
+          <a href="/Shop-category.html">
+            <img src="Assets/img/Shop images/Amplifiers.jpg" alt="Amplifiers" />
+            <figcaption>Amplifiers</figcaption>
+          </a>
+        </div>
+        <div class="grid-item">
+          <a href="/Shop-category.html">
+            <img src="Assets/img/Shop images/Speakers.jpg" alt="Speakers" />
+            <figcaption>Speakers</figcaption>
+          </a>
+        </div>
+        <div class="grid-item">
+          <a href="/Shop-category.html">
+            <img src="Assets/img/Shop images/Speakers.jpg" alt="Speakers" />
+            <figcaption>Speakers</figcaption>
+          </a>
+        </div>
+        <div class="grid-item">
+          <a href="/Shop-category.html">
+            <img src="Assets/img/Shop images/Turntables.jpg" alt="Turntables" />
+            <figcaption>Turntables</figcaption>
+          </a>
+        </div>
+        <div class="grid-item">
+          <a href="/Shop-category.html">
+            <img src="Assets/img/Shop images/CD Players.jpg" alt="CD Players" />
+            <figcaption>CD Players</figcaption>
+          </a>
+        </div>
+        <div class="grid-item">
+          <a href="/Shop-category.html">
+            <img src="Assets/img/Shop images/Streamers.jpg" alt="Streamers" />
+            <figcaption>Streamers</figcaption>
+          </a>
+        </div>
+        <div class="grid-item">
+          <a href="/Shop-category.html">
+            <img src="Assets/img/Shop images/Cables.jpg" alt="Cables" />
+            <figcaption>Cables</figcaption>
+          </a>
+        </div>
+        <div class="grid-item">
+          <a href="/Shop-category.html">
+            <img src="Assets/img/Shop images/Furniture.jpg" alt="Furniture" />
+            <figcaption>Furniture</figcaption>
+          </a>
+        </div>
+        <div class="grid-item">
+          <a href="/Shop-category.html">
+            <img src="Assets/img/Shop images/Headphones.jpg" alt="Headphones" />
+            <figcaption>Headphones</figcaption>
+          </a>
+        </div>
+        <div class="grid-item">
+          <a href="/Shop-category.html">
+            <img
+              src="Assets/img/Shop images/Home Cinema.jpg"
+              alt="Home Cinema"
+            />
+            <figcaption>Home Cinema</figcaption>
+          </a>
+        </div>
+        <div class="grid-item">
+          <a href="/Shop-category.html">
+            <img
+              src="Assets/img/Shop images/Outlet - Save Up to 5.jpg"
+              alt="Outlet"
+            />
+            <figcaption>Outlet</figcaption>
+          </a>
+        </div>
+        <div class="grid-item">
+          <a href="/Shop-category.html">
+            <img
+              src="Assets/img/Shop images/Ceiling Speakers.jpg"
+              alt="Ceiling Speakers"
+            />
+            <figcaption>Ceiling Speakers</figcaption>
+          </a>
+        </div>
+        <div class="grid-item">
+          <a href="/Shop-category.html">
+            <img src="Assets/img/Shop images/TVs.jpg" alt="TVs" />
+            <figcaption>TVs</figcaption>
+          </a>
+        </div>
+        <div class="grid-item">
+          <a href="/Shop-category.html">
+            <img
+              src="Assets/img/Shop images/Vinyl & LPs.jpg"
+              alt="Vinyl & LPs"
+            />
+            <figcaption>Vinyl & LPs</figcaption>
+          </a>
+        </div>
+        <div class="grid-item button-item">
+          <button class="shop-now">SHOP NOW</button>
+        </div>
       </section>
         </>
     )
