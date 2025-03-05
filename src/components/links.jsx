0c6c67ae555cccbdd5cc11882269971da9dd6fdc
Johndillerman <U2486892@rts-365.dk>
2025-03-05 14:14:18 +0100

    links

--- src/components/links.jsx
+++ src/components/links.jsx
@@ -2,63 +2,63 @@
 export default function Productlinks() {
   return (
     <>
-      <section class="grid-container" aria-label="Audio Shop Categories">
-        <div class="grid-item">
-          <a href="/Shop-category.html">
+      <section className="grid-container" aria-label="Audio Shop Categories">
+        <div className="grid-item">
+          <a href="">
             <img src="images/Shop images/Shop by Brand.jpg" alt="eBay Shop" />
             <figcaption>shop by brand</figcaption>
           </a>
         </div>
-        <div class="grid-item">
-          <a href="/Shop-category.html">
+        <div className="grid-item">
+          <a href="">
             <img src="images/Shop images/Amplifiers.jpg" alt="Amplifiers" />
             <figcaption>Amplifiers</figcaption>
           </a>
         </div>
-        <div class="grid-item">
-          <a href="/Shop-category.html">
+        <div className="grid-item">
+          <a href="">
             <img src="images/Shop images/Speakers.jpg" alt="Speakers" />
             <figcaption>Speakers</figcaption>
           </a>
         </div>
-        <div class="grid-item">
-          <a href="/Shop-category.html">
+        <div className="grid-item">
+          <a href="">
             <img src="images/Shop images/Turntables.jpg" alt="Turntables" />
             <figcaption>Turntables</figcaption>
           </a>
         </div>
-        <div class="grid-item">
-          <a href="/Shop-category.html">
+        <div className="grid-item">
+          <a href="">
             <img src="images/Shop images/CD Players.jpg" alt="CD Players" />
             <figcaption>CD Players</figcaption>
           </a>
         </div>
-        <div class="grid-item">
-          <a href="/Shop-category.html">
+        <div className="grid-item">
+          <a href="">
             <img src="images/Shop images/Streamers.jpg" alt="Streamers" />
             <figcaption>Streamers</figcaption>
           </a>
         </div>
-        <div class="grid-item">
-          <a href="/Shop-category.html">
+        <div className="grid-item">
+          <a href="">
             <img src="images/Shop images/Cables.jpg" alt="Cables" />
             <figcaption>Cables</figcaption>
           </a>
         </div>
-        <div class="grid-item">
-          <a href="/Shop-category.html">
+        <div className="grid-item">
+          <a href="">
             <img src="images/Shop images/Furniture.jpg" alt="Furniture" />
             <figcaption>Furniture</figcaption>
           </a>
         </div>
-        <div class="grid-item">
-          <a href="/Shop-category.html">
+        <div className="grid-item">
+          <a href="">
             <img src="images/Shop images/Headphones.jpg" alt="Headphones" />
             <figcaption>Headphones</figcaption>
           </a>
         </div>
-        <div class="grid-item">
-          <a href="/Shop-category.html">
+        <div className="grid-item">
+          <a href="">
             <img
               src="images/Shop images/Home Cinema.jpg"
               alt="Home Cinema"
@@ -66,8 +66,8 @@ export default function Productlinks() {
             <figcaption>Home Cinema</figcaption>
           </a>
         </div>
-        <div class="grid-item">
-          <a href="/Shop-category.html">
+        <div className="grid-item">
+          <a href="">
             <img
               src="images/Shop images/Outlet - Save Up to 5.jpg"
               alt="Outlet"
@@ -75,8 +75,8 @@ export default function Productlinks() {
             <figcaption>Outlet</figcaption>
           </a>
         </div>
-        <div class="grid-item">
-          <a href="/Shop-category.html">
+        <div className="grid-item">
+          <a href="">
             <img
               src="images/Shop images/Ceiling Speakers.jpg"
               alt="Ceiling Speakers"
@@ -84,14 +84,14 @@ export default function Productlinks() {
             <figcaption>Ceiling Speakers</figcaption>
           </a>
         </div>
-        <div class="grid-item">
-          <a href="/Shop-category.html">
+        <div className="grid-item">
+          <a href="">
             <img src="images/Shop images/TVs.jpg" alt="TVs" />
             <figcaption>TVs</figcaption>
           </a>
         </div>
-        <div class="grid-item">
-          <a href="/Shop-category.html">
+        <div className="grid-item">
+          <a href="">
             <img
               src="images/Shop images/Vinyl & LPs.jpg"
               alt="Vinyl & LPs"
@@ -99,7 +99,7 @@ export default function Productlinks() {
             <figcaption>Vinyl & LPs</figcaption>
           </a>
         </div>
-        <div class="grid-item button-item">
+        <div className="grid-item button-item">
           <button class="shop-now">SHOP NOW</button>
         </div>
       </section>
