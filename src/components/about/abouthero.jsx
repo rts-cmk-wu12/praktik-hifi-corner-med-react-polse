cd2fa4b247569b224f2e1394c4357a8046890fba
Johndillerman <U2486892@rts-365.dk>
2025-03-11 09:13:29 +0100

    about

---
+++ src/components/about/abouthero.jsx
@@ -0,0 +1,12 @@
+
+export default function Abouthero() {
+    return (
+        <>
+        <section className="hero">
+          <div className="hero-overlay">
+            <img src="/images/logo.svg" alt="Website Logo" />
+          </div>
+        </section>
+        </>
+    )
+}
